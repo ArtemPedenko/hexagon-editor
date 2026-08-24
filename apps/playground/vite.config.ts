3701e9eb333390d6f0af2c2b5967828027f12aa4
ArtemPedenko <nubostrel@gmail.com>
2026-08-24 16:40:45 +0300

    ci: deploy playground to GitHub Pages

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
+	base: '/hexagon-editor/',
     optimizeDeps: {
         include: ['hexagon-editor > mermaid', 'hexagon-editor > prosemirror-tables'],
     },
